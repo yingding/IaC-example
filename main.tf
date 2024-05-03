61675ebc91ac074bdc683fb5eec77e0e801c7ead
yingding <yingdingwang@yahoo.de>
2024-05-03 15:38:28 +0200

    update azure blob as ml datastore for azure ml studio

--- main.tf
+++ main.tf
@@ -29,7 +29,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.101.0" # "~>3.0.0" "=3.0.0" 
+      version = "~> 3.101.0" # "~>3.0.0" 
     }
   }
 }
@@ -115,4 +115,52 @@ resource "azurerm_machine_learning_workspace" "ml_workspace" {
   identity {
     type = "SystemAssigned"
   }
+}
+
+## Create a resource group on Azure with the variable name storage_rg_1 for storing sandbox data
+resource "azurerm_resource_group" "storage_rg_1" {
+  name     = "w1-test-data-rg-1"
+  location = "West Europe"
+
+  tags = {
+    zone = "sandbox"
+    owner = "yd"
+    app = "storage"
+  }
+}
+
+# storage account name can only consist of lowercase letters and numbers, 
+# and must be between 3 and 24 characters long
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/storage_account
+resource "azurerm_storage_account" "data_sa_1" {
+  name                     = "sandboxdatastorageacct1"
+  location                 = azurerm_resource_group.storage_rg_1.location
+  resource_group_name      = azurerm_resource_group.storage_rg_1.name
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  account_kind             = "StorageV2"
+  tags = azurerm_resource_group.storage_rg_1.tags 
+}
+
+# Create a Container object in the storage account
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/storage_container
+resource "azurerm_storage_container" "data_container_1" {
+  name                  = "data-container-1"
+  storage_account_name  = azurerm_storage_account.data_sa_1.name
+  container_access_type = "private" # "blob" #"private"
+  metadata = azurerm_resource_group.storage_rg_1.tags
+}
+
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/machine_learning_datastore_blobstorage
+# https://learn.microsoft.com/en-us/azure/templates/microsoft.machinelearningservices/workspaces/datastores?pivots=deployment-language-bicep
+resource "azurerm_machine_learning_datastore_blobstorage" "ml_blobstorage_1" {
+  name                 = "azureblob_datastore_1"
+  workspace_id         = azurerm_machine_learning_workspace.ml_workspace.id 
+  storage_container_id = azurerm_storage_container.data_container_1.resource_manager_id
+  account_key          = azurerm_storage_account.data_sa_1.primary_access_key # access key of the storage account
+  # shared_access_signature = {
+  #   expiry = "2023-12-31T00:00:00Z"
+  #   permissions = ["read", "write", "delete", "list"]
+  # }
+  tags = azurerm_resource_group.storage_rg_1.tags 
 }
